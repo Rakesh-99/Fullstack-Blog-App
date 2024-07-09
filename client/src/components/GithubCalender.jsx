5971fd7d4de488466ea78567407cee3d5d6c3042
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-07-10 00:23:23 +0530

    resize : resize github card width for wider product layout

--- client/src/components/GithubCalender.jsx
+++ client/src/components/GithubCalender.jsx
@@ -5,7 +5,7 @@ import { BsEmojiSmile } from "react-icons/bs";
 const GithubCalenderFunc = () => {
     return (
         <>
-            <div className="flex flex-col w-72 md:w-full items-center">
+            <div className="flex flex-col w-96 md:w-full items-center">
                 <div className="">
 
                     <p className='flex mb-5  justify-center font-semibold gap-2 text-xl'>Days I spend in coding
