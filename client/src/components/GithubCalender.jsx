ad389b63d19d17c34efee7924a552308f296415e
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-07-10 10:23:00 +0530

    resize : adjust width which was overflowing in small devices

--- client/src/components/GithubCalender.jsx
+++ client/src/components/GithubCalender.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import GithubCalender from 'react-github-calendar';
 import { BsEmojiSmile } from "react-icons/bs";
+import { useSelector } from 'react-redux';
+
+
 
 const GithubCalenderFunc = () => {
+
+    const { theme } = useSelector((state) => state.themeSliceApp);
+
     return (
         <>
-            <div className="flex flex-col w-96 md:w-full items-center">
+            <div className={`flex flex-col w-80 border rounded-md px-2 py-1 shadow-lg overflow-x-scroll scrollbar  md:w-full items-center ${theme === 'dark' ? 'border-gray-700' : 'border-gray-200'}`}>
                 <div className="">
 
                     <p className='flex mb-5  justify-center font-semibold gap-2 text-xl'>Days I spend in coding
