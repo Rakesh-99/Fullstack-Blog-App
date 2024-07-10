8f7cca14bb127e0e28e25a8565eae8eab933a7b2
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-07-11 02:56:32 +0530

    fix : fix nested p tag

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                         }}
                     />
 
-                    <p className='absolute flex-col top-20 md:ml-20 text-sm md:text-4xl md:left-80 left-52 flex gap-2 '
+                    <h1 className='absolute flex-col top-20 md:ml-20 text-sm md:text-4xl md:left-80 left-52 flex gap-2 '
                     >Hi, I am <motion.span className='bg-indigo-600 font-semibold py-1 text-xs md:text-4xl rounded-md  px-2'
 
                         initial={{ x: 30 }}
@@ -34,20 +34,20 @@ const About = () => {
 
                     >Rakesh Kumar Parida.</motion.span>
                         <span>Nice to meet you.</span>
-                    </p>
+                    </h1>
                 </div>
 
 
                 {/* Second Section  */}
                 <div className="flex min-h-screen w-full px-0 md:px-20 my-10">
-                    <p className='text-sm md:text-base leading-7 px-5  text-justify md:w-2/3'>
+                    <span className='text-sm md:text-base leading-7 px-5  text-justify md:w-2/3'>
                         I am a full-stack MERN developer with a deep understanding of front-end technologies, including <span className='font-semibold text-teal-400 '>HTML</span>, <span className='font-semibold text-teal-400 '>CSS</span> , <span className='font-semibold text-teal-400 '>JavaScript</span>, <span className='font-semibold text-teal-400 '>TailwindCss</span>, <span className='font-semibold text-teal-400 '>React Js</span>, <span className='font-semibold text-teal-400 '>Redux Toolkit</span>, and <span className='font-semibold text-teal-400 '>Framer Motion</span>. On the backend, I am proficient in <span className='font-semibold text-teal-400 '>Node.js</span> and <span className='font-semibold text-teal-400 '>Express.js</span> for creating <span className='font-semibold text-teal-400 '>server-side APIs</span>. I have experience with databases such as <span className='font-semibold text-teal-400 '>MongoDB</span> and <span className='font-semibold text-teal-400 '>MySQL</span>, and I am well-versed in version control systems like <span className='font-semibold text-teal-400 '>Git</span>. Additionally, I have knowledge of <span className='font-semibold text-teal-400 '>Core Java</span> and the <span className='font-semibold text-teal-400 '>Collection Framework</span>, with a strong grasp of <span className='font-semibold text-teal-400 '>OOPS</span> concepts.
 
 
                         <div className="my-10 flex justify-center ">
                             <GithubCalenderFunc />
                         </div>
-                    </p>
+                    </span>
 
                     <div className="">
                         <motion.img src={sideImg} alt="" className='w-80 md:block hidden'
